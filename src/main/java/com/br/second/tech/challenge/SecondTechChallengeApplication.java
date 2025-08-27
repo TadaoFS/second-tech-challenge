c7be2e3efa5ee226e563e141383541a4c8e34eb4
TadaoFS <Tads248624865@>
2025-08-27 14:50:53 -0300

    primeiro commit

---
+++ src/main/java/com/br/second/tech/challenge/SecondTechChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.br.second.tech.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondTechChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondTechChallengeApplication.class, args);
+	}
+
+}
