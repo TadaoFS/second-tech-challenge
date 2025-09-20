4df6ae0503a5656272ba2e884e69d7b03893a0e5
Fernando <fn4586@gmail.com>
2025-09-20 17:13:59 -0300

    rotas de usuarios

--- src/main/java/com/br/second/tech/challenge/infra/database/repository/UsuarioRepository.java
+++ src/main/java/com/br/second/tech/challenge/infra/database/repository/UsuarioRepository.java
@@ -1,9 +1,10 @@
 package com.br.second.tech.challenge.infra.database.repository;
 
 import com.br.second.tech.challenge.infra.database.entity.UsuarioEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UsuarioRepository extends CrudRepository <UsuarioEntity, Long> {
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 
 
 
